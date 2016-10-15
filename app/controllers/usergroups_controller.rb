f35ec38e85486491648e9ef26194a447ab6d2a98
ebdemoll <ebdem4@gmail.com>
2016-10-15 15:08:05 -0400

    changes for hound violations

--- app/controllers/usergroups_controller.rb
+++ app/controllers/usergroups_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 class UsergroupsController < ApplicationController
   def index
-  end 
+  end
 end
--- app/models/usergroup.rb
+++ app/models/usergroup.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 class Usergroup < ActiveRecord::Base
-
   validates :name, presence: true
-
 end

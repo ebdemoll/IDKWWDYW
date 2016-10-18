7142b78bb935eb33c6dfbe097a993fc3e86b9afe
ebdemoll <ebdem4@gmail.com>
2016-10-18 09:31:08 -0400

    created invite migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017183659) do
+ActiveRecord::Schema.define(version: 20161018132917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "invites", force: :cascade do |t|
+    t.string   "email"
+    t.integer  "user_group_id"
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.string   "token"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "memberships", force: :cascade do |t|
     t.integer  "user_id",      null: false

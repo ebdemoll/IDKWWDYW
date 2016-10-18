49d47a9146968316df60d4a477cc6ec7baf79563
ebdemoll <ebdem4@gmail.com>
2016-10-18 09:32:49 -0400

    created invite model and updated associations and routes

--- app/models/usergroup.rb
+++ app/models/usergroup.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class Usergroup < ActiveRecord::Base
+  has_many :invites
   has_many :memberships
   has_many :users, through: :memberships
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :invites
+
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 end

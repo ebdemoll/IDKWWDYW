8041b6cd72510b4984a1aa7bec6e2ff526c16919
ebdemoll <ebdem4@gmail.com>
2016-10-20 17:28:01 -0400

    removed http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-  config.action_mailer.default_url_options = { host: 'https://idkwdyw-nothing-fight.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'idkwdyw-nothing-fight.herokuapp.com' }
 
   # Do not dump schema after migrations.
   config.action_mailer.delivery_method = :smtp

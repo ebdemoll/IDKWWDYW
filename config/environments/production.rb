b0dc2c96eb5e4418165c9fe1104a11901f6b561c
ebdemoll <ebdem4@gmail.com>
2016-10-20 17:17:39 -0400

    removed sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,4 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-
 end
--- config/initializers/invite.rb
+++ config/initializers/invite.rb
@@ -1,14 +1,1 @@
-if Rails.env.development?
-  ActionMailer::Base.delivery_method = :sendmail
-  ActionMailer::Base.smtp_settings = { address: 'localhost', port: 1025 }
-else
-  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://idkwdyw-nothing-fight.herokuapp.com/',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-end
+

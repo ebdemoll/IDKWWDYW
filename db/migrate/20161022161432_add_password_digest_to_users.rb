67b2ab1bf2b36ff45c16e64496c0c61251460dca
ebdemoll <ebdem4@gmail.com>
2016-10-22 13:43:12 -0400

    migrations problems

--- db/migrate/20161022161432_add_password_digest_to_users.rb
+++ db/migrate/20161022161432_add_password_digest_to_users.rb
@@ -1,6 +1,5 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[5.0]
   def change
-    remove_column :users, :encrypted_password, :string
     remove_column :users, :salt, :string
     add_column :users, :name, :string
     add_column :users, :password_digest, :string
